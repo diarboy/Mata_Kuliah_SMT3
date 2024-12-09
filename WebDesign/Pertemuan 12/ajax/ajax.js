d2153613cab491cafd91c7831a878608793413e6
diarboy <bigdickdumb@gmail.com>
2024-12-09 11:51:19 +0700

    ajax

--- WebDesign/Pertemuan 12/ajax/ajax.js
+++ WebDesign/Pertemuan 12/ajax/ajax.js
@@ -2,12 +2,21 @@
 var xhr = new XMLHttpRequest();
 // Membuat GET untuk mengambil data
 xhr.open("GET",
-    "https://jsonplaceholder.typicode.com/posts", true);
+    "https://jsonplaceholder.typicode.com/posts?_limit=5", true); //hapus limit menampilkan semua
 // Membuat IF untuk menangani perubahan data server 
 xhr.onreadystatechange = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
         var responData = JSON.parse(xhr.responseText);
-        console.log(responData);
+        // console.log(responData);
+        // Menampilkan data ke element HTML
+        let container = document.getElementById("container");
+        responData.forEach((item) => {
+            const div = document.createElement("div");
+            div.innerHTML = `<h3>${item.title}</h3>
+            <p> ${item.body} </p>`;
+            container.appendChild(div);
+        });
     }
 };
+
 xhr.send();
